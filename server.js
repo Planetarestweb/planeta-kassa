792b6f5403fee646b8e05485597f37f78f595290
Planetarestweb <prosto.gama.05@gmail.com>
2026-05-15 13:48:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -54,6 +54,18 @@ async function ensureActiveShift() {
   }
   return shift;
 }
+// Разрешаем сайту кассы смены забирать данные
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
